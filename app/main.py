d74dd2bf28b1b2b8238255e78f059ae722b92730
Pablo Manzo <pabloo20012001@gmail.com>
2025-02-20 16:06:54 -0300

    refactor: update main.py to use router variable from router.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 from .database import engine, Base
-from .routes import urls
+from router import router
 
 app = FastAPI()
-app.include_router(urls.router)
+app.include_router(router)
 
 Base.metadata.create_all(bind=engine)
