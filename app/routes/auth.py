7b4f0c72d72f050742e96436072c175b91b4d229
Pablo Manzo <pabloo20012001@gmail.com>
2025-02-20 18:19:13 -0300

    refactor: separate logic from routes/auth into auth_logic, auth.py now handles only the endpoint

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,66 +1,11 @@
-from fastapi import APIRouter, Depends, HTTPException, status, Form
+from fastapi import APIRouter, Depends, HTTPException, Form
 from sqlalchemy.orm import Session
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from jose import JWTError, jwt
-from passlib.context import CryptContext
-from datetime import datetime, timezone, timedelta
-
+from fastapi.security import OAuth2PasswordRequestForm
+from app.auth_logic import create_access_token, hash_password, verify_password
 from app.database import get_db
 from app.models import User
 
 router = APIRouter()
-
-SECRET_KEY = "supersecret"
-ALGORITHM = "HS256"
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
-
-
-def hash_password(password: str):
-    return pwd_context.hash(password)
-
-
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
-
-
-def create_access_token(data: dict, expires_delta: int = 60):
-    to_encode = data.copy()
-    expire = datetime.now(timezone.utc) + timedelta(minutes=expires_delta)
-    to_encode.update({"exp": expire})
-    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-
-
-def verify_token(token: str, db: Session):
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
-            raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED,
-                detail="Could not validate credentials",
-            )
-        user = db.query(User).filter(User.username == username).first()
-        if user is None:
-            raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED,
-                detail="User not found",
-            )
-        return user
-    except JWTError:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Could not validate credentials",
-        )
-
-
-def verify_user_logged_in(
-    token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)
-):
-    user = verify_token(token, db)
-    return user
 
 
 @router.post("/register")
